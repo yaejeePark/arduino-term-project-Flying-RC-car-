ad3f21ad04921006d2e4eb98327ff0f077cc4cff
yaejeePark <yaejee89@gmail.com>
2017-11-18 23:03:22 +0900

    splash and main

---
+++ Flying_RC_car/app/src/main/java/com/example/yeaig/flying_rc_car/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.yeaig.flying_rc_car;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, SplashActivity.class));
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
